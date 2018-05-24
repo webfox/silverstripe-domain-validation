fd6885601ece599aa5ea35ba51554183890be4ac
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-05-24 07:02:51 +0000

    Fix some broken tests due to API changes

--- tests/CloudflareTest.php
+++ tests/CloudflareTest.php
@@ -5,24 +5,24 @@ namespace Codem\DomainValidation;
  * Tests for CloudflareDnsOverHttps
  */
 class CloudflareTest extends \SapphireTest {
-	
+
 	private static $mx_domain = "codem.com.au";
 	private static $a_answer = "codem.com.au.";
 	private static $mx_answer = "1 aspmx.l.google.com.";
-	
+
 	public function setUp() {
 		parent::setUp();
 	}
-	
+
 	public function testNoMx() {
-		
+
 		$validator = new CloudflareDnsOverHttps();
 		$validator->setDomain("example.com");
 		$answers = $validator->performLookup('MX');
 		// example.com should have no MX record
 		$this->assertFalse($answers);
 	}
-	
+
 	/**
 	 * Test that a domain has a specific MX record
 	 */
@@ -31,9 +31,9 @@ class CloudflareTest extends \SapphireTest {
 		$validator->setDomain( self::$mx_domain );
 		// Hopefully I don't change this regularly ;)
 		$has = $validator->hasMxRecord( self::$mx_answer );
-		$this->assertTrue($has);
+		$this->assertEquals( $has, self::$mx_answer );
 	}
-	
+
 	/**
 	 * Test that a domain has any MX records
 	 */
@@ -41,10 +41,10 @@ class CloudflareTest extends \SapphireTest {
 		$validator = new CloudflareDnsOverHttps();
 		$validator->setDomain( self::$mx_domain );
 		$has = $validator->hasMxRecord();
-		$this->assertTrue($has);
-		
+		$this->assertTrue( is_array($has) );
+
 	}
-	
+
 	/**
 	 * Test that a domain has any MX records
 	 */
@@ -53,7 +53,7 @@ class CloudflareTest extends \SapphireTest {
 		$validator->setDomain( self::$mx_domain );
 		$answers = $validator->performLookup('A');
 		$this->assertTrue( is_array($answers) );
-		
+
 		$valid = false;
 		foreach($answers as $answer) {
 			if(isset($answer->data) && $answer->name == self::$a_answer) {
@@ -62,7 +62,7 @@ class CloudflareTest extends \SapphireTest {
 		}
 		$this->assertTrue($valid);
 	}
-	
+
 	/**
 	 * Test failure condition
 	 */
@@ -70,7 +70,6 @@ class CloudflareTest extends \SapphireTest {
 		$validator = new CloudflareDnsOverHttps();
 		$validator->setDomain( self::$mx_domain );
 		$answers = $validator->performLookup('INVALID');
-		var_dump($answers);
 		$this->assertFalse($answers);
 	}
 }
--- tests/FieldValidationTest.php
+++ tests/FieldValidationTest.php
@@ -5,25 +5,25 @@ namespace Codem\DomainValidation;
  * Test validation of various addresses within {@link MxValidatedEmailField}
  */
 class EmailFieldTest extends \FunctionalTest {
-	
+
 	protected $valid_email_address= "devnull+gmailthing@codem.com.au";// valid domain has MX
 	protected $invalid_mx_address = "test@example.com";// valid domain that has no MX
 	protected $invalid_domain_email_address = "test@example.comau";// typo on the domain, but a valid email address
-	
+
 	protected $valid_domain = "codem.com.au";
 	protected $valid_example_domain = "example.com";
 	protected $invalid_domain = "example";
-	
+
 	public function testEmailValidation() {
-		
+
 		$emails = [
 			$this->valid_email_address => true,
 			$this->invalid_mx_address => false,
 			$this->invalid_domain_email_address => false,
 		];
-		
+
 		foreach($emails as $email => $expected_result) {
-			
+
 			$field = new MxValidatedEmailField("TestEmailAddress");
 			$field->setValue($email);
 
@@ -52,7 +52,7 @@ class EmailFieldTest extends \FunctionalTest {
 			}
 		}
 	}
-	
+
 	public function testDomainValidation() {
 
 		$domains = [
@@ -60,9 +60,9 @@ class EmailFieldTest extends \FunctionalTest {
 			$this->valid_example_domain => true,
 			$this->invalid_domain => false,
 		];
-		
+
 		foreach($domains as $domain => $expected_result) {
-			
+
 			$field = new ValidatedDomainField("TestDomain");
 			$field->setValue($domain);
 
@@ -90,7 +90,7 @@ class EmailFieldTest extends \FunctionalTest {
 				}
 			}
 		}
-		
+
 	}
 
 }
