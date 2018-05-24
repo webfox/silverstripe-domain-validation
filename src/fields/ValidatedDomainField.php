b23c894821080d25853dbe6ed9582ed8d001169a
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-05-24 07:05:07 +0000

    If there are no answers at all, ensure validation fails

--- src/fields/ValidatedDomainField.php
+++ src/fields/ValidatedDomainField.php
@@ -6,14 +6,14 @@ namespace Codem\DomainValidation;
  * This field does not check the existence of a domain name, as domain names can exist without any DNS records e.g an intranet not in DNS
  */
 class ValidatedDomainField extends \TextField implements FieldInterface {
-	
+
 	/**
 	 * {@inheritdoc}
 	 */
 	public function Type() {
 		return 'domainvalidated text';
 	}
-	
+
 	/**
 	 * @var array
 	 * @note one or more checks to perform. Can be A, AAAA, CNAME or anything else really
@@ -21,9 +21,9 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 	private static $dns_check = [
 		'A', // by default only do an A record check
 	];
-	
+
 	public $custom_dns_checks = [];
-	
+
 	/**
 	 * @var array
 	 * @note one or more domain validation class that extends Codem\DomainValidation\AbstractDomainValidator
@@ -31,20 +31,20 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 	private static $dns_validators = [
 		'Codem\DomainValidation\CloudflareDnsOverHttps',
 	];
-	
+
 	/**
 	 * Custom validators for custom validation
 	 * @var array
 	 * @param Codem\DomainValidation\AbstractDomainValidator $validator a domain validation class that extends Codem\DomainValidation\AbstractDomainValidator
 	 */
 	public $custom_validators = [];
-	
+
 	private $answers = [];
 	private $be_strict = false;
 	public function getAnswers() {
 		return $this->answers;
 	}
-	
+
 	/**
 	 * If true, any empty response will cause validate to return false
 	 */
@@ -52,7 +52,7 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 		$this->be_strict = $is;
 		return $this;
 	}
-	
+
 	public function validate($validator) {
 		$this->answers = [];
 		$lang_type = _t('DomainValidation.DOMAIN', 'domain');
@@ -68,20 +68,29 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 		if(!$result) {
 			return false;
 		}
-		
+
 		// assume that it's not valid
 		$validated = false;
 		try {
 			$result = $this->performDnsChecks($this->value, $validator, $lang_type);
 			$this->answers = $result;
+			if($this->value == "example") {
+				var_dump($result);
+			}
 			if($this->be_strict) {
 				$dns_checks = $this->getDnsChecks();
 				if(count($dns_checks) != count($this->answers)) {
 					throw new \Exception("Domain validation lookup did not return answers for all request checks");
+				} else {
+					$validated = true;
 				}
+			} else if(empty($this->answers)) {
+				// no results returned at all :(
+				throw new \Exception("No answers for requested DNS checks");
+			} else {
+				// not strict OR string AND all results returned
+				$validated = true;
 			}
-			// not strict OR string AND all results returned
-			$validated = true;
 		} catch (\Exception $e) {
 			\SS_Log::log("ERROR: " . $e->getMessage(), \SS_Log::INFO);
 			$message = sprintf(
@@ -89,7 +98,7 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 						$this->value
 			);
 		}
-		
+
 		if(!$validated) {
 			$validator->validationError(
 				$this->name,
@@ -98,6 +107,6 @@ class ValidatedDomainField extends \TextField implements FieldInterface {
 			);
 		}
 		return $validated;
-		
+
 	}
 }
